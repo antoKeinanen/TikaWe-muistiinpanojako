98b331ab7ab39eb405aa376e0d3826725a2150ca
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-20 17:08:23 +0200

    test: integration test authentication flow

---
+++ src/test/integration/test_authentication.py
@@ -0,0 +1,155 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestAuthentication(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(1)
+        self.driver.timeouts.page_load = 1
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_signin_successful(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signin")
+
+        driver.find_element(By.ID, "username").send_keys("user1")
+        driver.find_element(By.ID, "password").send_keys("Password123")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNotNone(driver.get_cookie("Authorization"))
+
+    def test_signin_fails_no_username(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signin")
+
+        driver.find_element(By.ID, "username").send_keys()
+        driver.find_element(By.ID, "password").send_keys("Password123")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+        self.assertIn("Käyttäjätunnus on pakollinen", errors)
+
+    def test_signin_fails_no_password(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signin")
+
+        driver.find_element(By.ID, "username").send_keys("user1")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+        self.assertIn("Salasana on pakollinen", errors)
+
+    def test_signin_fails_missing_csrf(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signin")
+
+        driver.execute_script('document.querySelector("#csrf_token").value = ""')
+        csrf_token_field = driver.find_element(By.ID, "csrf_token")
+        self.assertEqual(csrf_token_field.get_attribute("value"), "")
+
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+
+        self.assertIn("CSRF token puuttuu", errors)
+
+    def test_signin_fails_invalid_csrf(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signin")
+
+        driver.execute_script('document.querySelector("#csrf_token").value = "INVALID"')
+        csrf_token_field = driver.find_element(By.ID, "csrf_token")
+        self.assertEqual(csrf_token_field.get_attribute("value"), "INVALID")
+
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+
+        self.assertIn("CSRF tokenin validointi virhe", errors)
+
+    def test_signup_successful(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signup")
+
+        driver.find_element(By.ID, "username").send_keys("user2")
+        driver.find_element(By.ID, "password").send_keys("Password312")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNotNone(driver.get_cookie("Authorization"))
+
+    def test_signup_fails_no_username(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signup")
+
+        driver.find_element(By.ID, "username").send_keys()
+        driver.find_element(By.ID, "password").send_keys("Password123")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+        self.assertIn("Käyttäjätunnus on pakollinen", errors)
+
+    def test_signup_fails_no_password(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signup")
+
+        driver.find_element(By.ID, "username").send_keys("user1")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+        self.assertIn("Salasana on pakollinen", errors)
+
+    def test_signup_fails_missing_csrf(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signup")
+
+        driver.execute_script('document.querySelector("#csrf_token").value = ""')
+        csrf_token_field = driver.find_element(By.ID, "csrf_token")
+        self.assertEqual(csrf_token_field.get_attribute("value"), "")
+
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+
+        self.assertIn("CSRF token puuttuu", errors)
+
+    def test_signup_fails_invalid_csrf(self):
+        driver = self.driver
+        driver.get("http://localhost:5000/signup")
+
+        driver.execute_script('document.querySelector("#csrf_token").value = "INVALID"')
+        csrf_token_field = driver.find_element(By.ID, "csrf_token")
+        self.assertEqual(csrf_token_field.get_attribute("value"), "INVALID")
+
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        self.assertIsNone(driver.get_cookie("Authorization"))
+        errors = [
+            e.text for e in driver.find_elements(By.CLASS_NAME, "error-description")
+        ]
+
+        self.assertIn("CSRF tokenin validointi virhe", errors)
