607eddb14603b75901cbe31186fd204b061d62f3
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-28 16:56:56 +0200

    ref: clean up search_route

--- src/routes/search_route.py
+++ src/routes/search_route.py
@@ -5,9 +5,7 @@ from services import note_service
 
 @login_required
 def search_page():
-    query = flask.request.args.get("query")
-    if not query:
-        query = ""
+    query = flask.request.args.get("query", "")
 
-    notes, error = note_service.get_note_by_query(query)
+    notes = note_service.get_note_by_query(query)
     return flask.render_template("search.html", notes=notes, query=query)
