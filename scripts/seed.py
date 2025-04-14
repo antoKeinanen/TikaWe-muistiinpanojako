c8ab3b441c1c204e0e36107ea5a296f85e6b0f56
AntoKeinanen <anto.keinanen@gmail.com>
2025-04-14 15:56:46 +0300

    feat: add progress indicators to seeding script

--- scripts/seed.py
+++ scripts/seed.py
@@ -30,6 +30,8 @@ with sqlite3.connect("database.db") as db:
     cursor.executescript(db_init_script)
 
     for i in range(USER_COUNT):
+        if i % (USER_COUNT // 10) == 0:
+            print(f"{i}/{USER_COUNT} users added")  # noqa: T201
         password = secrets.token_urlsafe(32)
         token = secrets.token_urlsafe(32)
         cursor.execute(
@@ -38,6 +40,8 @@ with sqlite3.connect("database.db") as db:
         )
 
     for _ in range(NOTE_COUNT):
+        if i % (NOTE_COUNT // 10) == 0:
+            print(f"{i}/{NOTE_COUNT} notes added")  # noqa: T201
         user_id = random.randint(1, USER_COUNT)
         num_words = random.randint(5, 15)
         title = " ".join(random.choices(sanalista, k=2))
@@ -47,15 +51,19 @@ with sqlite3.connect("database.db") as db:
             (user_id, content, title),
         )
 
-    for note_id in range(1, NOTE_COUNT + 1):
-        for _ in range(3):
-            label = random.choice(sanalista)
-            cursor.execute(
-                "INSERT INTO tags (note_id, label) VALUES (?, ?)",
-                (note_id, label),
-            )
+    for i in range(TAG_COUNT):
+        if i % (TAG_COUNT // 10) == 0:
+            print(f"{i}/{TAG_COUNT} tags added")  # noqa: T201
+        note_id = random.randint(1, NOTE_COUNT)
+        label = random.choice(sanalista)
+        cursor.execute(
+            "INSERT INTO tags (note_id, label) VALUES (?, ?)",
+            (note_id, label),
+        )
 
-    for _ in range(COMMENT_COUNT):
+    for i in range(COMMENT_COUNT):
+        if i % (COMMENT_COUNT // 10) == 0:
+            print(f"{i}/{COMMENT_COUNT} comments added")  # noqa: T201
         user_id = random.randint(1, USER_COUNT)
         note_id = random.randint(1, NOTE_COUNT)
         num_words = random.randint(3, 20)
