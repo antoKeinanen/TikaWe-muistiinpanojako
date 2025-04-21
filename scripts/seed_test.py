17af72da79df866c80ccd1dceab9d1753245c9af
AntoKeinanen <anto.keinanen@gmail.com>
2025-04-21 19:48:24 +0300

    feat: remove external dependencies

--- scripts/seed_test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import sqlite3
-
-with Path("scripts/sql/seed_test.sql").open() as file:
-    seed_script = file.read()
-
-with sqlite3.connect("database.db") as connection:
-    cursor = connection.cursor()
-    cursor.executescript(seed_script)
